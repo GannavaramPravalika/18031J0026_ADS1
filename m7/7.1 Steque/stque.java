fd392f011d907cf10a9259810327efaf4c8b8b1b
GannavaramPravalika <pravalikagannavaram@gmail.com>
2018-10-22 14:52:18 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- m7/7.1 Steque/stque.java
+++ m7/7.1 Steque/stque.java
@@ -1,75 +1,85 @@
 
-public class stque{
-	//node class
-	Node head;
-	class Node
-	{
-		int data;
-		Node next;
-	}
-	//creating method for inserting a element
-	public void push(int p)
-	{
-		//creating a new node
-		Node n=new Node();
-		//send the data to the node
-		n.data=p;
-		n.next=null;
-		//traversing through the list
-		if(head!=null)
-		{
-			n.next=head;
-		}
-		head=n;
-	}
-	//method for removing the element
-	public void pop()
-	{
-		//creating a temporarary node
-		Node t=head;
-		head=t.next;
-	}
-	//method to add an element at the end 
-	public void enque(int r)
-	{
-		Node n1=new Node();
-		n1.data=r;
-		n1.next=null;
-		Node temp=head;
-		while(temp.next!=null)
-		{
-			temp=temp.next;
-		}
-		temp.next=n1;
-	}
-	public void display()
-	{
-		Node t1=head;
-		while(t1.next!=null)
-		{
-			System.out.println(t1.data);
-			t1=t1.next;
-		}
-		System.out.println(t1.data);
-	}
-	public static void main(String args[])
-	{
-		stque st=new stque();
-		st.push(10);
-		st.push(15);
-		//st.display();
-		st.push(45);
-		//st.display();
-		st.pop();
-		//st.display();
-		st.enque(35);
-		//st.display();
-		st.enque(84);
-		st.display();
-	}
-	
-	
-	
-	
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Steque1 {
+	LinkedList<Integer> ll;
+	public Steque1(){
+		ll = new LinkedList<Integer>();
+	}
+
+	public void push(int item) {
+		ll.addFirst(item);
+	}
+
+	public String pop() {
+		return (ll.isEmpty())?"Steque is empty.":(""+ll.removeFirst());
+		
+	}
+
+	public void enqueue(int item) {
+		ll.addLast(item);
+	}
+
+	public String print() {
+
+		if (ll.isEmpty()) {
+
+			return "Steque is empty.";
+		}
+		String st = "" + ll.get(0);
+		for (int i = 1; i < ll.size(); i++) {
+			st = st + ", " + ll.get(i);
+		}
+		return st;
+
+	}
+
+	public void removeAll() {
+		// TODO Auto-generated method stub
+		ll = new LinkedList<Integer>();
+	}
+}
+
+public class Solution
+{
+
+	public static void main(String[] args) {
+		int n, data;
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		Steque1 st = new Steque1();
+		int flag = 0;
+		do {
+			String s = scan.nextLine();
+			String[] s1 = s.split(" ");
+			switch (s1[0]) {
+			case "enqueue":
+				data = Integer.parseInt(s1[1]);
+				st.enqueue(data);
+				System.out.println(st.print());
+				break;
+			case "push":
+				data = Integer.parseInt(s1[1]);
+				st.push(data);
+				System.out.println(st.print());
+				break;
+			case "pop":
+				st.pop();
+				System.out.println(st.print());
+				break;
+			default:
+				st.removeAll();
+				if(flag==1)
+				System.out.println();
+				flag=1;
+
+			}
+
+		} while (scan.hasNext());
+
+	 }
+
+	
 }
