41eb87781f798f76be97919eef0231e73280a8da
GannavaramPravalika <pravalikagannavaram@gmail.com>
2018-11-09 17:38:37 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m19/Module 19 - Ordered operations in BSTs/19.1 BST/BinaryTree.java
+++ m19/Module 19 - Ordered operations in BSTs/19.1 BST/BinaryTree.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Scanner;
 
 class Book{
@@ -30,15 +31,23 @@ class Node
 	}
 }
 
-public class Solution {
+public class BinaryTree {
 	Node root;
+	
+	public BinaryTree() {
+		this.root = null;
+	}
+
+	static int indx=0;
 	public void put(Book key,int val) {
 		root=put(root,key,val);
 	}
 	public Node put(Node node,Book key,int val) {
 		if(node==null) {
+			//System.out.println("T1:"+key);
 			return new Node(key,val);
 		}
+		
 		int cmp=key.Name.compareTo(node.key.Name);
 		if(cmp<0) {
 			node.left=put(node.left,key,val);
@@ -59,6 +68,9 @@ public class Solution {
 		}
 		
 	}
+	
+	
+	
 
 	public Node Floor(Node x,Book key) {
 		if(x==null)
@@ -75,7 +87,7 @@ public class Solution {
 		}
 			return x;
 	}
-	public Node Ceiling(Node x,Book key) {
+	public Node Ceiling(Node x,Book key) { 
 		if(x==null)
 			return x;
 		int c=key.Name.compareTo(x.key.Name);
@@ -105,6 +117,23 @@ public class Solution {
         }
         return node.key.toString();
     }
+	public void Select(int k) {
+		
+		select(root,k);
+	}
+	
+	public void select(Node temp,int sval) {
+		if(temp!=null) {
+			select(temp.left,sval);
+			if(indx==sval) {
+				System.out.println(temp.key);
+				indx++;
+				return;
+			}
+			indx++;
+			select(temp.right,sval);
+		}
+	}
  
 	public int get(Book Key)
 	{
@@ -121,6 +150,51 @@ public class Solution {
 		}
 		return 0;
 	}
+	public void deletemin() {
+		root=deletemin(root);
+	}
+	public Node deletemin(Node x) {
+		if(x.left==null) {
+			return x.right;
+		}else
+			x.left=deletemin(x.left);
+		return x;
+	}
+	public void deletemax() {
+		root=deletemax(root);
+	}
+	public Node deletemax(Node x) {
+		if(x.right==null) {
+			return x.left;
+		}else
+			x.right=deletemax(x.right);
+		return x;
+	}
+
+	public void delete(Book key) {
+		root = delete(root, key);
+	}
+
+	public Node delete(Node x, Book key) {
+		if (x == null)
+			return null;
+		int cmp = key.Name.compareTo(x.key.Name);
+		if (cmp < 0)
+			x.left = delete(x.left, key);
+		else if (cmp > 0)
+			x.right = delete(x.right, key);
+		else {
+			if (x.right == null)
+				return x.left;
+			if (x.left == null)
+				return x.right;
+			Node t = x;
+			x = deletemin(t.right);
+			x.right = deletemin(t.right);
+			x.left = t.left;
+		}
+		return x;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -129,15 +203,17 @@ public class Solution {
 		while(svn.hasNext()) {
 			String s1=svn.nextLine();
 			String s2[]=s1.split(",");
+			Node temp;
+			Book b=null;
+			if(s2.length>2)
+				b = new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
 			switch (s2[0]) {
 			case "put":
-				Book b=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
 				bt.put(b, Integer.parseInt(s2[4]));
 				break;
 			case "get":
-				Book b1=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
-				if(bt.get(b1)!=0)
-					System.out.println(bt.get(b1));
+				if(bt.get(b)!=0)
+					System.out.println(bt.get(b));
 				else
 				 	System.out.println("null");
 				break;
@@ -145,18 +221,30 @@ public class Solution {
 				bt.print(bt.root);
 				break;
 			case "floor":
-				Book b2=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
-				System.out.println(bt.Floor(bt.root,b2).key);
+				temp = bt.Floor(bt.root,b);
+				System.out.println(temp==null?null:temp.key);
 				break;
 			case "ceiling":
-				Book b3=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
-				System.out.println(bt.Ceiling(bt.root,b3).key);
+				temp = bt.Ceiling(bt.root,b);
+				System.out.println(temp==null?null:temp.key);
 				break;
 			case "min":
 				System.out.println(bt.minValue(bt.root));
 				break;
 			case "max":
 				System.out.println(bt.maxValue(bt.root));
+				break;
+			case "select":
+				bt.Select(Integer.parseInt(s2[1]));
+				break;
+			case "deleteMin":
+				bt.deletemin();
+				break;
+			case "deleteMax":
+				bt.deletemax();
+				break;
+			case "delete":
+				bt.delete(b);
 				break;
 			}
 			
@@ -165,4 +253,3 @@ public class Solution {
 	}
 
 }
-
