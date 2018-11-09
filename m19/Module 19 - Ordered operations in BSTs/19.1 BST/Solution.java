1759e0df25c57e624d5ec6408407418f51804cde
GannavaramPravalika <pravalikagannavaram@gmail.com>
2018-11-09 17:53:39 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m19/Module 19 - Ordered operations in BSTs/19.1 BST/Solution.java
+++ m19/Module 19 - Ordered operations in BSTs/19.1 BST/Solution.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Scanner;
 
 class Book{
@@ -31,23 +29,16 @@ class Node
 	}
 }
 
-public class BinaryTree {
+public class Solution {
 	Node root;
-	
-	public BinaryTree() {
-		this.root = null;
-	}
-
 	static int indx=0;
 	public void put(Book key,int val) {
 		root=put(root,key,val);
 	}
 	public Node put(Node node,Book key,int val) {
 		if(node==null) {
-			//System.out.println("T1:"+key);
 			return new Node(key,val);
 		}
-		
 		int cmp=key.Name.compareTo(node.key.Name);
 		if(cmp<0) {
 			node.left=put(node.left,key,val);
@@ -150,70 +141,23 @@ public class BinaryTree {
 		}
 		return 0;
 	}
-	public void deletemin() {
-		root=deletemin(root);
-	}
-	public Node deletemin(Node x) {
-		if(x.left==null) {
-			return x.right;
-		}else
-			x.left=deletemin(x.left);
-		return x;
-	}
-	public void deletemax() {
-		root=deletemax(root);
-	}
-	public Node deletemax(Node x) {
-		if(x.right==null) {
-			return x.left;
-		}else
-			x.right=deletemax(x.right);
-		return x;
-	}
-
-	public void delete(Book key) {
-		root = delete(root, key);
-	}
-
-	public Node delete(Node x, Book key) {
-		if (x == null)
-			return null;
-		int cmp = key.Name.compareTo(x.key.Name);
-		if (cmp < 0)
-			x.left = delete(x.left, key);
-		else if (cmp > 0)
-			x.right = delete(x.right, key);
-		else {
-			if (x.right == null)
-				return x.left;
-			if (x.left == null)
-				return x.right;
-			Node t = x;
-			x = deletemin(t.right);
-			x.right = deletemin(t.right);
-			x.left = t.left;
-		}
-		return x;
-	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner svn=new Scanner(System.in);
-		BinaryTree bt=new BinaryTree();
+		Solution bt=new Solution();
 		while(svn.hasNext()) {
 			String s1=svn.nextLine();
 			String s2[]=s1.split(",");
-			Node temp;
-			Book b=null;
-			if(s2.length>2)
-				b = new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
 			switch (s2[0]) {
 			case "put":
+				Book b=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
 				bt.put(b, Integer.parseInt(s2[4]));
 				break;
 			case "get":
-				if(bt.get(b)!=0)
-					System.out.println(bt.get(b));
+				Book b1=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
+				if(bt.get(b1)!=0)
+					System.out.println(bt.get(b1));
 				else
 				 	System.out.println("null");
 				break;
@@ -221,12 +165,12 @@ public class BinaryTree {
 				bt.print(bt.root);
 				break;
 			case "floor":
-				temp = bt.Floor(bt.root,b);
-				System.out.println(temp==null?null:temp.key);
+				Book b2=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
+				System.out.println(bt.Floor(bt.root,b2).key);
 				break;
 			case "ceiling":
-				temp = bt.Ceiling(bt.root,b);
-				System.out.println(temp==null?null:temp.key);
+				Book b3=new Book(s2[1],s2[2],Double.parseDouble(s2[3]));
+				System.out.println(bt.Ceiling(bt.root,b3).key);
 				break;
 			case "min":
 				System.out.println(bt.minValue(bt.root));
@@ -236,15 +180,6 @@ public class BinaryTree {
 				break;
 			case "select":
 				bt.Select(Integer.parseInt(s2[1]));
-				break;
-			case "deleteMin":
-				bt.deletemin();
-				break;
-			case "deleteMax":
-				bt.deletemax();
-				break;
-			case "delete":
-				bt.delete(b);
 				break;
 			}
 			
